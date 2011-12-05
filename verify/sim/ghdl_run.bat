d5813821eabb389efa721ed3d4825e8342be2a1a
brimdavis <brimdavis@gmail.com>
2011-12-05 02:30:05 +0000

    sync

--- verify/sim/ghdl_run.bat
+++ verify/sim/ghdl_run.bat
@@ -1,3 +1,10 @@
-ghdl -r    --workdir=work.ghd  --ieee=synopsys  testbench --stop-time=20000ns > ghdl_sim.out
+::
+:: run simulation using GHDL
+::
+
+:: ieee-asserts option not present in GHDL 0.25
+::ghdl -r   --workdir=work.ghd  --ieee=synopsys  testbench --ieee-asserts=disable-at-0  --stop-time=20000ns > ghdl_sim.out
+
+ghdl -r   --workdir=work.ghd  --ieee=synopsys  testbench  --stop-time=20000ns > ghdl_sim.out
 
 
