9c49f81f46dade6d9820a33e4f7b17b0d5d9f367
brimdavis <brimdavis@gmail.com>
2012-02-04 22:27:09 +0000

    added <pcr_calc.vhd> for LDI/EA/return address calculations using pc_reg_p1 delayed calls should work now, need to add test cases

---
+++ hdl/cores/y1a/pcr_calc.vhd
@@ -0,0 +1,116 @@
+-- 
+-- <pcr_calc.vhd>
+-- 
+
+---------------------------------------------------------------
+--
+-- (C) COPYRIGHT 2012  Brian Davis
+--
+-- Code released under the terms of the BSD 2-clause license
+-- see license/bsd_2-clause.txt
+--
+---------------------------------------------------------------
+
+--
+-- Y1A PC Relative address calculations
+--
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+  use ieee.std_logic_unsigned."+";
+  use ieee.std_logic_unsigned."-";
+
+library work;
+  use work.y1_constants.all;
+  use work.y1a_config.all;
+
+  use work.y1a_probe_pkg.all;
+
+
+entity pcr_calc is
+
+  port
+    (   
+      inst_fld       : in  std_logic_vector(ID_MSB downto 0);
+      sel_opb        : in  std_logic_vector(3 downto 0);
+
+      dslot          : in  std_logic;
+
+      call_type      : in  std_logic;
+      ext_bit        : in  std_logic;
+      ext_grp        : in  std_logic_vector(3 downto 0);
+
+      ldi_offset     : in  std_logic_vector(11 downto 0);
+
+      pc_reg_p1      : in  std_logic_vector(PC_MSB downto 0);
+    
+      pcr_addr       : out std_logic_vector(ALU_MSB downto 0)
+    );
+
+end pcr_calc;
+
+
+architecture arch1 of pcr_calc is
+
+  attribute syn_hier : string;
+  attribute syn_hier of arch1: architecture is "hard";
+
+  signal pc_p1_masked   : std_logic_vector(ALU_MSB downto 0);
+  signal pcr_off_mux    : std_logic_vector(ALU_MSB downto 0);
+  signal ea_pc_lsbs     : std_logic_vector(1 downto 0);
+
+  signal dcd_LDI     : boolean;
+  signal dcd_CALL    : boolean;
+
+begin
+
+  --
+  -- instruction decodes
+  --
+  dcd_LDI  <= ( inst_fld = OPM_LDI );
+
+  dcd_CALL <= (
+                    ( (inst_fld = OPC_EXT) AND (ext_bit = '1' ) AND ( ext_grp = EXT_JUMP ) )
+                OR  ( inst_fld = OPC_BR )
+              )
+              AND ( call_type = '1' );
+
+
+  --
+  -- mux pcr offset sources
+  --
+  pcr_off_mux <=  
+ 
+          ( ( ALU_MSB downto 14 => '0') & ldi_offset & B"00" )  
+    when  dcd_LDI
+
+    --
+    -- return address calculation:
+    --   add 2 when dslot = 0
+    --   add 4 when dslot = 1
+    --
+    else  ( ALU_MSB downto 3 => '0' ) & dslot & (NOT dslot) & '0'
+    when  dcd_CALL
+
+    else  ( others => '0')
+    ;
+
+
+  --
+  -- LSB's of PC zeroed for LDI quad aligned addressing
+  --
+  ea_pc_lsbs   <= B"00" when dcd_LDI else pc_reg_p1(1 downto 0);
+
+  pc_p1_masked <= ( ALU_MSB downto PC_MSB+1 => '0') & pc_reg_p1(PC_MSB downto 2)  & ea_pc_lsbs;
+--    when  dcd_LDI OR ( ( sel_opb = REG_PC ) AND ( NOT dcd_SP ) )
+
+
+  --
+  -- PC Relative adder
+  --
+  pcr_addr  <=  pcr_off_mux + pc_p1_masked;
+
+
+end arch1;
--- verify/control/bsr1/bsr1.s
+++ verify/control/bsr1/bsr1.s
@@ -3,7 +3,7 @@
 ;
 
 ;
-; (C) COPYRIGHT 2001-2011  Brian Davis
+; (C) COPYRIGHT 2001-2012  Brian Davis
 ;
 ; Code released under the terms of the BSD 2-clause license
 ; see license/bsd_2-clause.txt
@@ -28,11 +28,11 @@
         mov     r0, #0
 
 ; start stacking up subroutine calls
-        bsr     sub1
+        bsr.d   sub1
+        xor     r0,#$8000_0000
 
-        nop     ; verify hack
         nop     
-        .verify r0,#$0000_FFFF
+        .verify r0,#$8000_FFFF
 
 done:
         bra     done
@@ -41,8 +41,8 @@ done:
 sub1:
         nop
 
-        or      r0,#$0000_0001
-        .verify r0,#$0000_0001
+        xor     r0,#$0000_0001
+        .verify r0,#$8000_0001
 
         bsr     sub2
 
@@ -52,7 +52,7 @@ sub2:
         nop
 
         or      r0,#$0000_0002
-        .verify r0,#$0000_0003
+        .verify r0,#$8000_0003
 
         bsr     sub3
 
@@ -62,7 +62,7 @@ sub3:
         nop
 
         or      r0,#$0000_0004
-        .verify r0,#$0000_0007
+        .verify r0,#$8000_0007
 
         bsr     sub4
 
@@ -72,7 +72,7 @@ sub4:
         nop
 
         or      r0,#$0000_0008
-        .verify r0,#$0000_000f
+        .verify r0,#$8000_000f
 
         bsr     sub5
 
@@ -82,7 +82,7 @@ sub5:
         nop
 
         or      r0,#$0000_0010
-        .verify r0,#$0000_001f
+        .verify r0,#$8000_001f
 
         bsr     sub6
 
@@ -92,7 +92,7 @@ sub6:
         nop
 
         or      r0,#$0000_0020
-        .verify r0,#$0000_003f
+        .verify r0,#$8000_003f
 
         bsr     sub7
 
@@ -102,7 +102,7 @@ sub7:
         nop
 
         or      r0,#$0000_0040
-        .verify r0,#$0000_007f
+        .verify r0,#$8000_007f
 
         bsr     sub8
 
@@ -112,7 +112,7 @@ sub8:
         nop
 
         or      r0,#$0000_0080
-        .verify r0,#$0000_00ff
+        .verify r0,#$8000_00ff
 
         bsr     sub9
 
@@ -122,7 +122,7 @@ sub9:
         nop
 
         or      r0,#$0000_0100
-        .verify r0,#$0000_01FF
+        .verify r0,#$8000_01FF
 
         bsr     sub10
 
@@ -132,7 +132,7 @@ sub10:
         nop
 
         or      r0,#$0000_0200
-        .verify r0,#$0000_03FF
+        .verify r0,#$8000_03FF
 
         bsr     sub11
 
@@ -142,7 +142,7 @@ sub11:
         nop
 
         or      r0,#$0000_0400
-        .verify r0,#$0000_07FF
+        .verify r0,#$8000_07FF
 
         bsr     sub12
 
@@ -152,7 +152,7 @@ sub12:
         nop
 
         or      r0,#$0000_0800
-        .verify r0,#$0000_0FFF
+        .verify r0,#$8000_0FFF
 
         bsr     sub13
 
@@ -162,7 +162,7 @@ sub13:
         nop
 
         or      r0,#$0000_1000
-        .verify r0,#$0000_1FFF
+        .verify r0,#$8000_1FFF
 
         bsr     sub14
 
@@ -172,7 +172,7 @@ sub14:
         nop
 
         or      r0,#$0000_2000
-        .verify r0,#$0000_3FFF
+        .verify r0,#$8000_3FFF
 
         bsr     sub15
 
@@ -182,7 +182,7 @@ sub15:
         nop
 
         or      r0,#$0000_4000
-        .verify r0,#$0000_7FFF
+        .verify r0,#$8000_7FFF
 
         bsr     sub16
 
@@ -192,7 +192,7 @@ sub16:
         nop     ; verify hack
 
         or      r0,#$0000_8000
-        .verify r0,#$0000_FFFF
+        .verify r0,#$8000_FFFF
 
         rts
 
