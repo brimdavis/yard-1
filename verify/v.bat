c8546fbd23d67d6da57f64ea006574e3d1bc06e3
brimdavis <brimdavis@gmail.com>
2012-08-20 00:51:51 +0000

    simulation subdirectory reorg: added common batch file to create file lists for sim & core targets

--- verify/v.bat
+++ verify/v.bat
@@ -1,21 +1,33 @@
 ::
-:: verify script, needs path to subdirectory containing test
+:: verify script, call with path to subdirectory containing test
 ::
-call yas    %1\%~n1.s     || exit /b 1
-
-call ylink  %1\%~n1.obj   || exit /b 1
-
-call ymovit %1\%~n1       || exit /b 1
 
 ::
-:: cd to the proper directory to run the simulator defined by %YARD_SIM%
+:: assemble, link, copy object file to simulation target directory
+::
+call yas     %1\%~n1.s    || exit /b 1
+call ylink   %1\%~n1.obj  || exit /b 1
+call ymovit  %1\%~n1      || exit /b 1
+
+::
+:: build file lists for simulation run
+::
+call sim\build_file_lists.bat
+
+::
+:: cd to the simulation directory defined by %YARD_SIM%
 ::
 cd sim\%YARD_SIM%
 
+::
+:: compile and run model
+::
 call sim_comp || exit /b 1
-
 call sim_run  || exit /b 1
 
+::
+:: check against expected results
+::
 cd ..\..
 
 copy sim\%YARD_SIM%\sim.out %1\%YARD_SIM%_sim.out
