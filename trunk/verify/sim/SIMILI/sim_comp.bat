2dfff605c729233a63082de4759cfb2e8695bab0
brimdavis@gmail.com <brimdavis@gmail.com@5c06d173-e226-0410-aa07-233efb3e3bf4>
2012-08-20 00:51:51 +0000

    simulation subdirectory reorg: added common batch file to create file lists for sim & core targets

--- trunk/verify/sim/SIMILI/sim_comp.bat
+++ trunk/verify/sim/SIMILI/sim_comp.bat
@@ -1,13 +1,7 @@
-set YC=%YARD_HOME%\HDL\cores\y1a\
-set YV=%YARD_SIM_TARGET%\
-
-:: needs work to automatically compile using local file list for each target
-:: manually select for now
-
-:: sim_rtl directory
-vhdlp %YC%y1a_config.vhd %YC%y1_constants.vhd %YC%y1a_comps.vhd %YC%y1a_probe_pkg.vhd %YC%pw2_rom.vhd %YC%addsub.vhd %YC%logicals.vhd %YC%shift_one.vhd %YC%flip.vhd %YC%ffb.vhd %YC%bitcnt.vhd %YC%rstack.vhd %YC%rf.vhd %YC%cgen.vhd %YC%ea_calc.vhd %YC%pcr_calc.vhd %YC%dbus_ctl.vhd %YC%st_mux.vhd %YC%ld_mux.vhd %YC%skip_dcd.vhd %YC%y1a_probe.vhd %YC%y1a_core.vhd %YV%rom_dat.vhd %YV%rtl_mem.vhd %YV%evb.vhd %YV%evb_tb.vhd
-
-:: sim_bram_4K directory
-:: vhdlp %YC%y1a_config.vhd %YC%y1_constants.vhd %YC%y1a_comps.vhd %YC%y1a_probe_pkg.vhd %YC%pw2_rom.vhd %YC%addsub.vhd %YC%logicals.vhd %YC%shift_one.vhd %YC%flip.vhd %YC%ffb.vhd %YC%bitcnt.vhd %YC%rstack.vhd %YC%rf.vhd %YC%cgen.vhd %YC%ea_calc.vhd %YC%pcr_calc.vhd %YC%dbus_ctl.vhd %YC%st_mux.vhd %YC%ld_mux.vhd %YC%skip_dcd.vhd %YC%y1a_probe.vhd %YC%y1a_core.vhd %YV%stub_blkram.vhd %YV%rom_init.vhd %YV%block_ram.vhd YV%evb_tb.vhd
+::
+:: compile model
+::   simulator file lists are now built from a common batch file
+::
+vhdlp %YC_FILES% %YV_FILES%
 
 if errorlevel 1 exit /b 1
