75b5eac0f8b7179310631d42c9dad73553bca70c
brimdavis <brimdavis@gmail.com>
2013-03-24 03:09:16 +0000

    build_ise_9v2 : add files needed for command line build

---
+++ hdl/systems/evb_digilent_s3_starter/build_ise_9v2/build.bat
@@ -0,0 +1,24 @@
+::
+::
+::
+cd temp
+
+::xst -intstyle xflow   -ifn ..\evb.xst -ise evb.prj -ofn evb.syr 
+xst -intstyle xflow   -ifn ..\evb.xst -ofn evb.syr 
+if errorlevel 1 exit /b 1
+
+ngdbuild -intstyle xflow -dd _ngo -nt timestamp -insert_keep_hierarchy -uc ..\..\evb.ucf -p xc3s200-ft256-4 evb.ngc evb.ngd
+if errorlevel 1 exit /b 1
+
+map  -intstyle xflow     -p xc3s200-ft256-4 -timing -logic_opt on -ol high -xe n -t 1 -cm area -detail -l -pr b -k 4 -power off -o evb_map.ncd evb.ngd evb.pcf
+if errorlevel 1 exit /b 1
+
+par  -intstyle xflow     -w -ol high -xe n -t 1 evb_map.ncd evb.ncd evb.pcf
+if errorlevel 1 exit /b 1
+
+trce -intstyle xflow     -v 3 -s 4 -xml evb evb.ncd -o evb.twr evb.pcf -ucf ..\..\evb.ucf
+if errorlevel 1 exit /b 1
+
+bitgen   -f ..\evb.ut evb.ncd
+
+cd ..
---
+++ hdl/systems/evb_digilent_s3_starter/build_ise_9v2/set_xilinx_path.bat
@@ -0,0 +1,8 @@
+::
+:: set Xilinx tool path 
+::  - use for older Xilinx releases 
+::  - newer releases provide a settings{32|64}.bat in the Xilinx install directory 
+::
+set PATH=%PATH%;C:\Xilinx_WP92\bin\nt\;
+set XILINX=C:\Xilinx_WP92
+
