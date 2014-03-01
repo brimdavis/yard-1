8e2ff0d3bc345b2b71477d28740b0b96b65310dd
brimdavis <brimdavis@gmail.com>
2014-03-01 02:39:47 +0000

    doc/development_software.txt : updated links
    yen.bat : derive project path from batch file location ( i.e. can now run yen.bat from a different working directory to setup YARD tools)

--- yen.bat
+++ yen.bat
@@ -3,14 +3,27 @@
 ::
 :: sets up project environment variables and path
 ::
-:: run this script from the top level project directory
+:: run this script once in each new command shell before using the tools
+::
+:: note: avoid spaces in the installation path, some of the invoked project batch files might choke on spaces
 ::
 
-set YARD_HOME=%CD%
+::
+:: working-directory-insensitive method of determining project home from the location of this batch file
+::
+::   %~dp0 means dRIVE pATH argument0
+::
+::   note that %~dp0 includes a trailing slash, which is then removed on the next line
+::
+set YARD_HOME=%~dp0
+set YARD_HOME=%YARD_HOME:~0,-1%
 
+::
+:: add command line tools to PATH
+::
 set YARD_TOOLS=%YARD_HOME%\tools
 
-set path=%path%;%YARD_TOOLS%\winxp;
+set PATH=%PATH%;%YARD_TOOLS%\winxp;
 
 ::
 :: TODO: need to pick a PERL here ( check for perl install, bail to xilperl )
@@ -18,13 +31,21 @@ set path=%path%;%YARD_TOOLS%\winxp;
 set YARD_PERL=perl
 
 ::
-:: select a VHDL simulator 
+:: select a VHDL simulator by name
+::
+::
+::  YARD_SIM     Company/
+::  Name         Organization      Product
+:: ---------------------------------------------
 ::
 :: Currently Supported:
+::
 ::  GHDL         GHDL              GHDL
 ::  SIMILI       Symphony EDA      VHDL Simili
 ::
+::
 :: Planned:
+::
 ::  MODELSIM     Model Technology  Modelsim
 ::  ALDEC        Aldec             Active-HDL or Riviera
 ::  ISIM         Xilinx            Isim
