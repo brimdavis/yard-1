3659f961e4c447d8a4f58db984077cdb3d811e0c
brimdavis <brimdavis@5c06d173-e226-0410-aa07-233efb3e3bf4>
2014-09-30 01:40:55 +0000

    m_uart/test :  - added baud rate monitor to testbench  - commenting & cleanup

--- hdl/cores/m_uart/test/m_uart_tb.vhd
+++ hdl/cores/m_uart/test/m_uart_tb.vhd
@@ -34,15 +34,17 @@
 --
 -- simulation testbench for m_uart
 --
---  - sets up stimulus for manual inspection
+--  - writes known data pattern to UART transmitter
 --
---  - simple check of rx data against tx data
+--  - checks RX data against TX data
 --
---  - TODO: read and write procedures instead of in-line code
+--  - checks baud rate within 0.5 %
+--
+--
+--  - TODO: read procedure instead of in-line code
 --
 --  - TODO: use behavioral UART model for checking instead of loopback
 --
-
 
 library ieee;
   use ieee.std_logic_1164.all;
@@ -56,19 +58,46 @@ end testbench;
 
 architecture bench1 of testbench is
 
+  -------------------------
+  --
+  -- TB control constants
+  --
   constant TB_CLK_PERIOD : time := 20 ns;
-  constant T_HOLD        : time :=  2 ns;
+  constant TB_HOLD       : time :=  2 ns;      -- testbench hold time for DUT stimulus
 
+  constant TB_RUN_TIME   : time := 25 ms;
 
+  constant TB_BAUD       : real := 115_200.0;
+
+  --
+  -- make queue size large enough to accomodate all transmited bytes sent by testbench
+  --
+  constant MAX_QUEUE_SIZE : natural := 1024;
+
+  -------------------------
+
+  --
+  -- compute clock frequency used for baud calculations from TB_CLK_PERIOD 
+  --
+  constant TB_CLK_FREQ   : real := 1.0 / ( real( TB_CLK_PERIOD / 1 ns ) * 1.0e-9 );
+
+  --
+  -- type defs
+  --
   subtype byte    is std_logic_vector(7 downto 0);
 
   type byte_array is array ( natural range <> ) of byte;
 
-
+  --
+  -- tb control signals
+  --
   signal tb_run   : std_logic := 'L';
 
   signal clk      : std_logic := 'L';
 
+  --
+  -- signals for DUT wiring
+  --
   signal baud_16x : std_logic;   
 
   signal tx_rdy   : std_logic;   
@@ -86,11 +115,14 @@ architecture bench1 of testbench is
   --
   -- array to record write data for comparison
   --
-  signal qdat     : byte_array( 0 to 1023 );
-  signal qti      : integer := 0;
-  signal qri      : integer := 0;
+  signal qdat     : byte_array( 0 to MAX_QUEUE_SIZE-1 );   -- Queued DATa
+  signal qti      : integer := 0;                          -- Queue Transmit Index
+  signal qri      : integer := 0;                          -- Queue Receive Index
+
 
   signal rx_errs  : integer := 0;
+
+  signal last_baud_freq  : real := 0.0;
 
 
 --
@@ -99,14 +131,15 @@ architecture bench1 of testbench is
 begin
 
   --
-  -- generate clock; gated with tb_run to end simulation
+  -- generate clock
+  --   gated with tb_run to end simulation by stopping the clock
   --
-  clk    <= (NOT clk) AND tb_run after TB_CLK_PERIOD/2;
+  clk <= (NOT clk) AND tb_run after TB_CLK_PERIOD/2;
 
   --
-  -- run testbench for 25 ms 
+  -- run testbench for desired interval 
   --
-  tb_run <= '1', '0' after 25 ms;
+  tb_run <= '1', '0' after TB_RUN_TIME;
 
   --
   -- instantiate UART 
@@ -136,8 +169,8 @@ begin
   E_baud_gen: entity work.simple_baud_gen
     generic map
       (
-        CLK_FREQ  => 50_000_000.0,
-        BAUD_RATE =>    115_200.0
+        CLK_FREQ  => TB_CLK_FREQ,
+        BAUD_RATE => TB_BAUD
       )
     port map
       (        
@@ -151,9 +184,9 @@ begin
   --
   rx_bit <= transport tx_bit after 1 ms;
 
-  
+
   --
-  --
+  -- transmit data
   --
   tb_uart_tx : process
 
@@ -164,20 +197,34 @@ begin
 
       begin
 
+        --
+        -- loop until uart transmitter is ready
+        --   note synchronous sampling of transmit ready line 
+        --
         loop
           wait until rising_edge(clk);
           exit when tx_rdy = '1';
         end loop;
 
+        --
+        -- synchronously assert data & write strobe 
+        --
         wait until rising_edge(clk);
-          tx_dat    <= wdat  after T_HOLD;
-          uart_wr   <= '1'   after T_HOLD;
+          tx_dat  <= wdat  after TB_HOLD;
+          uart_wr <= '1'   after TB_HOLD;
 
+          --
+          -- record transmit data in queue for later comparison
+          --
           qdat(qti) <= wdat;
           qti       <= qti + 1;
 
+        --
+        -- synchronously deassert write strobe 
+        --
         wait until rising_edge(clk);
-          uart_wr <= '0'   after T_HOLD;
+          tx_dat  <= ( others => '0') after TB_HOLD;
+          uart_wr <= '0'              after TB_HOLD;
 
       end;
 
@@ -204,11 +251,11 @@ begin
       -- overwrite test - write while busy, byte should be ignored
       --
       wait until rising_edge(clk);
-        tx_dat  <= X"FF" after T_HOLD;
-        uart_wr <= '1'   after T_HOLD;
+        tx_dat  <= X"FF" after TB_HOLD;
+        uart_wr <= '1'   after TB_HOLD;
 
       wait until rising_edge(clk);
-        uart_wr <= '0'   after T_HOLD;
+        uart_wr <= '0'   after TB_HOLD;
 
       --
       -- send another byte 
@@ -222,7 +269,9 @@ begin
         send_byte( std_logic_vector(to_unsigned(i,8)) );
       end loop;
 
-
+      --
+      -- done, wait forever
+      --
       wait;
 
     end process tb_uart_tx;
@@ -242,51 +291,96 @@ begin
       wait for 100 ns;
 
       loop
-        --
-        -- wait for rx_rdy flag
-        --
-        if rx_rdy = '0' then
-          wait until rx_rdy = '1';
-        end if;
 
         --
-        -- read data
+        -- loop until uart receiver has data
+        --   note synchronous sampling of receiver ready line 
         --
-        wait until rising_edge(clk);
-        uart_rd <=   '1' after T_HOLD;
-
-        --
-        -- deassert read
-        --
-        wait until rising_edge(clk);
-        uart_rd <=   '0' after T_HOLD;
-
-        --
-        -- read data at next clock edge
-        --
-        wait until rising_edge(clk);
+        loop
+          wait until rising_edge(clk);
+          exit when rx_rdy = '1';
+        end loop;
 
         --
         -- check that rx = tx
+        --   note that receive data output is valid coincident with receive ready flag assertion
         --
         if rx_dat /= qdat(qri) then
-
+  
           rx_errs <= rx_errs + 1;
 
-          assert rx_dat = qdat(qri) 
+          assert FALSE
             report "TX/RX data mismatch"
             severity ERROR;
-
 
         end if;
 
         qri <= qri + 1;
 
+        --
+        -- pulse the read signal for one clock cycle
+        --
+        wait until rising_edge(clk);
+          uart_rd <=   '1' after TB_HOLD;
+
+        wait until rising_edge(clk);
+          uart_rd <=   '0' after TB_HOLD;
+
       end loop;
 
+      --
+      -- done, wait forever
+      --
       wait;
 
     end process tb_uart_rx;
+
+  --
+  -- monitor baud rate generator
+  --
+  tb_baud_mon : process
+
+    variable first_edge, last_edge : time;
+
+    variable baud_freq    : real := 0.0;
+
+    constant NAVG         : natural := 100;
+
+    begin
+
+      --
+      -- measure elapsed time for NAVG baud ticks
+      --
+      wait until rising_edge(baud_16x);
+      wait until rising_edge(clk);
+      first_edge := now;
+
+      for i in 1 to NAVG loop
+        wait until rising_edge(baud_16x);
+        wait until rising_edge(clk);
+      end loop;
+
+      last_edge := now;
+
+      --
+      -- convert to real baud rate, accounting for NAVG samples at 16x baud rate
+      --  note, using time scale < 1ns might overflow integer time conversion
+      --
+      baud_freq := real(NAVG) / ( real( (last_edge - first_edge) / 1 ns ) * 1.0e-9 * 16.0 );
+
+      --
+      -- check frequency accuracy within 0.5 %
+      --
+      assert ( baud_freq/TB_BAUD <= 1.005 ) AND ( baud_freq/TB_BAUD >= 0.995 )
+        report  "Baud rate error > 0.5 %  (" & real'image(baud_freq) & ")"
+        severity ERROR;
+
+      --
+      -- copy latest measurement to global testbench signal
+      --
+      last_baud_freq <= baud_freq;
+
+    end process;
 
 
   --
@@ -295,9 +389,11 @@ begin
   P_tb_summary : process
     begin
 
-      wait until tb_run = '1';
+      wait until falling_edge(tb_run);
 
-      wait until tb_run = '0';
+      assert FALSE
+        report "Measured baud rate = " & real'image(last_baud_freq)
+        severity NOTE;
 
       assert FALSE
         report  "Data Comparison Errors: " & integer'image(rx_errs)
